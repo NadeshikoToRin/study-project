64a38b14b429a59c2e97fbf871c9c67731ace1b6
CyberLelouch <167730646@qq.com>
2024-09-21 20:35:57 +0800

    000-init project

---
+++ study-project-backend/src/test/java/com/example/studyprojectbackend/StudyProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
